dd2c0ce20736cf6f6fa7c37d35017f910d026f7c
Andreas Heinen <andreas.heinen@posteo.de>
2022-03-23 21:03:18 +0100

    just testing push

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,9 @@ export default function Home() {
               <StyledListActualAmount>
                 {item.actualAmount}
               </StyledListActualAmount>
+              {/* <StyledListActualAmount>
+                {item.actualAmount}
+              </StyledListActualAmount> */}
             </StyledItem>
           );
         })}
