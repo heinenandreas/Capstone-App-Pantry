6557a0b3e8ee449020f2f811d41dcf332439fae3
Andreas Heinen <andreas.heinen@posteo.de>
2022-03-24 11:18:19 +0100

    added min and max amount for products

---
+++ components/GlobalStyle/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+      box-sizing: border-box;
+      --darkblue: #2d2861;
+      --orange: #fe7902;
+      --lightyellow: #fcc13f;
+      --green: #fe7902;
+      --lightgreen: #8ad234;
+      --pink: #d6007e;
+      --neonpink: #fc008b;
+      margin: 0;
+}
+
+:root {
+      }
+`;
