39819d7d950f24a449a0b6d6c4eea87e8642141c
Andreas Heinen <andreas.heinen@posteo.de>
2022-03-29 13:36:19 +0200

    install SWR, add db.js, add schema folder

---
+++ schema/Product.js
@@ -0,0 +1,12 @@
+import { Schema, model } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    text: { type: String, required: true, minlength: 5 },
+  },
+  { timestamps: true }
+);
+
+export default model("Product", jokeSchema, "products", {
+  overwriteModels: true,
+});
