66dc132b4977568b29572aa8d801e3ab928b6958
Andreas Heinen <andreas.heinen@posteo.de>
2022-04-08 10:18:16 +0200

    configure next auth

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import { useSession, signIn, signOut } from "next-auth/react";
 import Category from "../components/Category/Category";
 
 export default function Home() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        <Category />
+      </>
+    );
+  }
   return (
     <>
-      <Category />
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
     </>
   );
 }
